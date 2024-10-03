59e7d7a6df2be0096b26b9e0d5bd84933240d89e
pakshaA <95148744+pakshaA@users.noreply.github.com>
2024-10-03 11:30:44 +0300

    Update config.js

--- app/api/config.js
+++ app/api/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://pindie-back.vercel.app//api';
+export const BASE_URL = 'https://pindie-back.vercel.app/api';
 
 export const endpoints = {
     games: `${BASE_URL}/games`,
